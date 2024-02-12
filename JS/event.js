e8fa92a39ab8a3497aa0f7b9663655b780ad6e9f
prosenjithdashpappu <prosenjith.edu@gmail.com>
2024-02-13 03:18:18 +0600

    Done addEvnetLisener

--- JS/event.js
+++ JS/event.js
@@ -1,1 +1,44 @@
-console.log('Event js learn.')
+// option: 02
+
+// function makeYellow() {
+//     document.body.style.background = 'yellow';
+// }
+
+
+
+//#################################
+
+
+// <!-- Option: 3 -->
+
+// //Make blue with id
+
+//     const makeBlueButton = document.getElementById('make_blue');
+//         makeBlueButton.onclick = makeBlue;
+
+//         function makeBlue(){
+//                 document.body.style.backgroundColor = 'blue';
+//             }
+
+
+
+//##################################
+
+
+// < !--option : 4 -- >
+
+//         const pinkButton = document.getElementById('make_pink')
+//         // console.log(pinkButton)
+//         pinkButton.addEventListener('click', makePink)
+
+//         function makePink(){
+//             document.body.style.backgroundColor = 'pink';
+//         }
+
+// Option 4 final
+        // we will use that
+
+        // document.getElementById('make_orange').addEventListener('click', function makeOrange() { document.body.style.backgroundColor = 'orange';})
+        // // function makeOrange(){
+        // //     document.body.style.backgroundColor = 'orange';
+        // // }
